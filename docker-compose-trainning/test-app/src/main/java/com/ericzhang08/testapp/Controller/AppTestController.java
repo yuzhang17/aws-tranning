f2eb4f7f6348b9be060babcd5b166f7788a697d3
ericzhang08 <yuzhangfish@163.com>
2020-05-07 12:59:13 +0800

    add maven cache image

--- docker-compose-trainning/test-app/src/main/java/com/ericzhang08/testapp/Controller/AppTestController.java
+++ docker-compose-trainning/test-app/src/main/java/com/ericzhang08/testapp/Controller/AppTestController.java
@@ -3,11 +3,9 @@ package com.ericzhang08.testapp.Controller;
 
 import lombok.*;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
